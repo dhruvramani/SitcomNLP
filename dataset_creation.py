64d48eb1c1ac93a35b3c2a12d20a35dbdd2b307d
dhruvramani <dhruvramani98@gmail.com>
2017-12-29 20:37:11 +0530

    Video2Laugh Complete

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,13 +1,21 @@
 import os
+import pickle
 from pydub import AudioSegment
 from pydub.playback import play
 import moviepy.editor as mp
+from sound_classification import SoundClassification
 
 def vidtomp3(filename):
     # SOURCE : https://stackoverflow.com/questions/33448759/python-converting-video-to-audio
     clip = mp.VideoFileClip(filename)
     clip.audio.write_audiofile("{}_audio.mp3".format(filename))
     return "{}_audio.mp3".format(filename)
+
+def vidtowav(filename):
+    # SOURCE : https://stackoverflow.com/questions/26741116/python-extract-wav-from-video-file
+    command = "ffmpeg -i ./data/videos/{}.mp4 -ab 160k -ac 2 -ar 44100 -vn ./data/audio/{}_audio.wav".format(filename, filename)
+    os.system(command)
+    return "./data/audio/{}_audio.wav".format(filename)
 
 def remvocal(filename):
     # SOURCE : https://stackoverflow.com/questions/3673042/algorithm-to-remove-vocal-from-sound-track
@@ -20,6 +28,23 @@ def remvocal(filename):
     sound_CentersOut.export("{}_novocal".format(filename), format="mp3")
     return "{}_novocal.mp3".format(filename)
 
+def detect_laughter(wavfile, train=False):
+    
+    _SOUND_OBJ = "./models/sco.sav"
+    _DATSET_PATH = "./data/laugh_dataset"
 
+    if(train == False):
+        sco = pickle.load(open(_SOUND_OBJ), "rb")
+    else:
+        file_regexp = os.path.join(_DATSET_PATH, '*.wav')
+        files = glob.glob(file_regexp)
+        sco = SoundClassification(wav_file_list=files, calibrate_score=True)
+        sco.learn()
+        pickle.dump(sco, open(_SOUND_OBJ, "wb"))
 
+    res = sco.processed_wav(wavfile)
+    print([x for x in res])
 
+if __name__ == '__main__':
+    wavpath = vidtowav("S08E01")
+    detect_laughter(wavpath, True)
