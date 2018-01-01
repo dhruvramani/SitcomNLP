75562e912ae0c2a2720ec7184d8850f12e6a6534
dhruvramani <dhruvramani98@gmail.com>
2018-01-01 15:46:24 +0530

    Dataset Processing done

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,9 +1,27 @@
 import os
+import time
 import pickle
-from pydub import AudioSegment
-from pydub.playback import play
+from datetime import datetime
+#from pydub import AudioSegment
+#from pydub.playback import play
 import moviepy.editor as mp
-from sound_classification import SoundClassification
+
+def get_subsentence(timestep, filepath):
+    with open(filepath, "r") as f:
+        text = f.read().split("\n\n")
+        for i in text:
+            contents = i.split("\n")
+            times = i[1].split(" --> ")
+            timestr1, timestr2 = times[0].split(',')[0], times[1].split(',')[0]
+            time1, time2 = datetime.strptime(timestr1, "%H:%M:%S"), datetime.strptime(timestr2, "%H:%M:%S")
+            if(time1 <= timestep and timestep <= time2):
+                return contents[2:]
+
+def get_sentence(subsentence, filepath):
+    with open(filepath, "r" ) as f:
+        text = f.read().split("\n\n")
+        return [sent for sent in text if subsentence in sent][0]
+
 
 def vidtomp3(filename):
     # SOURCE : https://stackoverflow.com/questions/33448759/python-converting-video-to-audio
@@ -17,6 +35,7 @@ def vidtowav(filename):
     os.system(command)
     return "./data/audio/{}_audio.wav".format(filename)
 
+'''
 def remvocal(filename):
     # SOURCE : https://stackoverflow.com/questions/3673042/algorithm-to-remove-vocal-from-sound-track
     myAudioFile = AudioSegment.from_mp3(filename)
@@ -27,24 +46,28 @@ def remvocal(filename):
     sound_CentersOut = sound_monoL.overlay(sound_monoR_inv)
     sound_CentersOut.export("{}_novocal".format(filename), format="mp3")
     return "{}_novocal.mp3".format(filename)
+'''
 
-def detect_laughter(wavfile, train=False):
-    
-    _SOUND_OBJ = "./models/sco.sav"
-    _DATSET_PATH = "./data/laugh_dataset"
+def detect_laughter(wavfilepath):
 
-    if(train == False):
-        sco = pickle.load(open(_SOUND_OBJ), "rb")
-    else:
-        file_regexp = os.path.join(_DATSET_PATH, '*.wav')
-        files = glob.glob(file_regexp)
-        sco = SoundClassification(wav_file_list=files, calibrate_score=True)
-        sco.learn()
-        pickle.dump(sco, open(_SOUND_OBJ, "wb"))
+    return timesteps
 
-    res = sco.processed_wav(wavfile)
-    print([x for x in res])
+def main(datasetpath, vidfilepath, subfilepath, transpath):
+    wavpath = vidtowav(vidfilepath)
+    timesteps = detect_laughter(wavfilepath)
+    sentences = list()
+    for timestep in timesteps:
+        subsentences = get_subsentence(timestep, subfilepath)
+        sentence = get_sentence(subsentences, transpath)
+        sentences.append(sentence)
+
+    with open(datasetpath, "w") as datf:
+        with open(transpath, "r") as transf:
+            for line in transf:
+                if(line in sentences):
+                    line = line.rstrip()
+                    line += " LAUGH LAUGH\n"
+                datf.write(line)
 
 if __name__ == '__main__':
     wavpath = vidtowav("S08E01")
-    detect_laughter(wavpath, True)
