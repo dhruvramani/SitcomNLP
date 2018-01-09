cfba1ca510607212d35e113dd4ffeb043fcfb8cd
dhruvramani <dhruvramani98@gmail.com>
2018-01-09 13:41:39 +0530

    working

--- dataset_2.py
+++ dataset_2.py
@@ -35,30 +35,32 @@ def unicodetoascii(text):
 # TODO if not found go to next 4 words
 def get_timestamp(subtext, to_search):
     try :
-        return [x for x in subtext if unicodetoascii(to_search) in " ".join(x.split("\n")[2:])][0].split("\n")[1].split(" --> ")
+        x =  [x for x in subtext if unicodetoascii(to_search) in " ".join(x.split("\n")[2:])][0]
+        return x.split("\n")[1].split(" --> ")
     except :
-        print(to_search)
         return ["-1", "-1"]
         
 def search_words(sentence, subtext):
+    sentence = unicodetoascii(sentence)
     init_timestamp, end_timestamp = "-1", "-1"
+    word_count = 7
     words = sentence.split(" ")
-    word_count = 5
-    while("-1" == init_timestamp and word_count > 1):
+    ending = ""
+    while("-1" in [init_timestamp, end_timestamp] and word_count > 1):
         starting = " ".join(words[:word_count])
-        init_timestamp = get_timestamp(subtext, starting)[0]
-        word_count -= 1
-    word_count = 5
-    while("-1" == end_timestamp and word_count > 1):
         ending = " ".join(words[-word_count:])
-        end_timestamp = get_timestamp(subtext, ending)[1]
+        if(init_timestamp == "-1"):
+            init_timestamp = get_timestamp(subtext, starting)[0]
+        if(end_timestamp == "-1"):
+            end_timestamp = get_timestamp(subtext, ending)[1]
         word_count -= 1
-    #print(init_timestamp, end_timestamp)
+
     if(init_timestamp == "-1" or end_timestamp == "-1"):
         return "-1", "-1", -1, -1
-    init_timestamp, end_timestamp = init_timestamp.split(",")[0], end_timestamp.split(",")[0]
+    #init_timestamp, end_timestamp = init_timestamp.split(",")[0], end_timestamp.split(",")[0]
     start_ind = [subtext.index(x) for x in subtext if init_timestamp in x][-1]
     final_ind = [subtext.index(x) for x in subtext if end_timestamp in x][-1]
+
     return init_timestamp, end_timestamp, start_ind, final_ind
 
 # CHANGE : subtitle -> text -> till text : subtitle
@@ -84,12 +86,11 @@ def transcripttimestamp(transpath, subpath, newpath):
                     pass
                 else :
                     _, _, start_ind, final_ind = search_words(sent, subtext)
-                    #print(sent)
                     if(-1 in [start_ind, final_ind]):
                         continue
                     old_init, old_end = subtext[start_ind].split("\n")[1].split(" --> ")
                     reset, count, wholesent = 0, 0, " ".join([x for x in subtext[start_ind].split("\n")[2:] if x in sent])
-                    for i in range(start_ind + 1, final_ind):
+                    for i in range(start_ind + 1, final_ind + 1):
                         new_init, new_end = subtext[i].split("\n")[1].split(" --> ")
                         sec1, sec2 = int(new_init.split(":")[2].split(",")[0]), int(old_end.split(":")[2].split(",")[0])
                         if(sec1 - sec2 >= 1):
@@ -113,8 +114,8 @@ def transcripttimestamp(transpath, subpath, newpath):
     return output
 
 if __name__ == '__main__':
-    transpath = "./small.txt"                                                                                                                
-    subpath = "./small.srt"                                                                                                                    
+    transpath = "./data/transcripts/S08E01.txt"                                                                                                                
+    subpath = "./data/subtitles/s08e01.srt"                                                                                                                    
     newpath = "./new.txt"  
     os.system("rm -rf {}".format(newpath))                                                                                                                              
     transcripttimestamp(transpath, subpath, newpath)
