f25693ffe944153e900612fea4b18145121358e3
dhruvramani <dhruvramani98@gmail.com>
2018-01-14 12:30:01 +0530

    BBT Ready

--- dataset_2.py
+++ dataset_2.py
@@ -35,7 +35,7 @@ def unicodetoascii(text):
 
 regex1 = re.compile(r'[^A-Za-z0-9\s]+')
 regex2 = re.compile(r'\([^)]*\)')
-# TODO if not found go to next 4 words
+
 def format_string(strin):
     sentence = strin.lower()
     sentence = unicodetoascii(sentence)
@@ -59,7 +59,6 @@ def search_words(sentence, subtext):
     while("-1" in [init_timestamp, end_timestamp] and word_count > 1):
         starting = " ".join(words[:word_count])
         ending = " ".join(words[-word_count:])
-        print(" | ".join([sentence, starting, ending]), "\n\n")
         if(init_timestamp == "-1"):
             init_timestamp = get_timestamp(subtext, starting)[0]
         if(end_timestamp == "-1"):
@@ -68,13 +67,12 @@ def search_words(sentence, subtext):
 
     if(init_timestamp == "-1" or end_timestamp == "-1"):
         return "-1", "-1", -1, -1
-    #init_timestamp, end_timestamp = init_timestamp.split(",")[0], end_timestamp.split(",")[0]
+
     start_ind = [subtext.index(x) for x in subtext if init_timestamp in x][-1]
     final_ind = [subtext.index(x) for x in subtext if end_timestamp in x][-1]
 
     return init_timestamp, end_timestamp, start_ind, final_ind
 
-# CHANGE : subtitle -> text -> till text : subtitle
 def transcripttimestamp(transpath, subpath, newpath):
     """Returns a dictionary of format dict[sentence] = [initial_timestamp, ending_timestamp, speaker]
 
@@ -88,7 +86,6 @@ def transcripttimestamp(transpath, subpath, newpath):
         with open(subpath, "r") as sub:
             transtext = str(trans.read()).split("\n\n")
             subtext = str(sub.read()).split("\n\n")
-            #init_timestamp, end_timestamp = "-1", "-1"
             new = open("./new.txt", "w+")
             for senti in transtext:
                 if(":" not in senti):
@@ -103,7 +100,6 @@ def transcripttimestamp(transpath, subpath, newpath):
                     old_init, old_end = subtext[start_ind].split("\n")[1].split(" --> ")
                     reset, count, wholesent = 0, 0, " ".join([x for x in subtext[start_ind].split("\n")[2:] if x in sent])
                     speaker = senti.split(":")[0]
-                    #print(senti, foo, bar, end="\n\n")
                     for i in range(start_ind + 1, final_ind + 1):
                         new_init, new_end = subtext[i].split("\n")[1].split(" --> ")
                         sec1, sec2 = int(new_init.split(":")[2].split(",")[0]), int(old_end.split(":")[2].split(",")[0])
@@ -112,8 +108,6 @@ def transcripttimestamp(transpath, subpath, newpath):
                             speaker = senti.split(":")[0]
                             to_put = [speaker, wholesent, init_timestamp, end_timestamp]
                             new.write("{}\n\n".format(to_put))
-                            #output.append(to_put)
-                            #print(to_put, speaker)
                             wholesent = " ".join(subtext[i].split("\n")[2:])
                             old_init, old_end = new_init, new_end
                         else :
@@ -124,14 +118,9 @@ def transcripttimestamp(transpath, subpath, newpath):
                     speaker = senti.split(":")[0]
                     to_put = [speaker, wholesent, init_timestamp, end_timestamp]
                     new.write("{}\n\n".format(to_put))
-                    #output.append()
                     wholesent = ""
             new.close()
 
-
-
-    ##with open(newpath, "w+") as new:
-    ##    new.write("\n\n".join([" | ".join(sent) for sent in output]))
             
     return output
 
